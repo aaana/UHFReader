f3317754675d552b39d3b77051d0f204469f60e8
Anna Huang <593880978@qq.com>
2017-10-23 20:07:21 +0800

    提交utils

---
+++ WindowsFormsApp3/WindowsFormsApp3/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace WindowsFormsApp3
+{
+    class Utils
+    {
+        public static string listToString(List<int> entries)
+        {
+            string sortingEntries = "";
+            foreach (int i in entries)
+            {
+                sortingEntries += i + ",";
+            }
+            if (sortingEntries.Length > 0)
+            {
+                sortingEntries = sortingEntries.Substring(0, sortingEntries.Length - 1);
+            }
+            return sortingEntries;
+        }
+    }
+}
